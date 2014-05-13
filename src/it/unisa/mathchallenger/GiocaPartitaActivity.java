33787521851b39613157fddf318556f46e76eff8
pinoelefante <pinoelefante>
2014-05-13 19:09:08 +0200

    aggiunto menu all'activity gioca partita modificato il manifest in modo da assegnare come label delle attività il nome dell'applicazione

--- src/it/unisa/mathchallenger/GiocaPartitaActivity.java
+++ src/it/unisa/mathchallenger/GiocaPartitaActivity.java
@@ -2,6 +2,7 @@ package it.unisa.mathchallenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 
 
@@ -11,5 +12,11 @@ public class GiocaPartitaActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_gioca_partita);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.gioca_partita, menu);
+		return true;
 	}
 }
