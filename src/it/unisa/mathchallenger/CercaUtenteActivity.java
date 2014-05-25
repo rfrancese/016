4a9b9a1059acecd4ff335532448e55c58a4f6d0b
pinoelefante <pinoelefante@hotmail.it>
2014-05-25 19:34:06 +0200

    rimosso Settings dal menu

--- src/it/unisa/mathchallenger/CercaUtenteActivity.java
+++ src/it/unisa/mathchallenger/CercaUtenteActivity.java
@@ -65,15 +65,13 @@ public class CercaUtenteActivity extends ActionBarActivity {
 		return true;
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
 		return super.onOptionsItemSelected(item);
 	}
 
