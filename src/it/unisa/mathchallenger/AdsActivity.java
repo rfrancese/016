13a7d51d65c4077b549f12c148fc461b2c6c17a3
pinoelefante <pinoelefante@hotmail.it>
2014-05-29 17:25:20 +0200

    commentato a data da destinarsi

--- src/it/unisa/mathchallenger/AdsActivity.java
+++ src/it/unisa/mathchallenger/AdsActivity.java
@@ -20,62 +20,62 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 
 public class AdsActivity extends ActionBarActivity {
-	  /** The view to show the ad. */
-	  private AdView adView;
-
-	  /* Your ad unit id. Replace with your actual ad unit id. */
-	  private static final String AD_UNIT_ID = "INSERT_YOUR_AD_UNIT_ID_HERE";
-
-	  /** Called when the activity is first created. */
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.);
-
-	    // Create an ad.
-	    adView = new AdView(this);
-	    adView.setAdSize(AdSize.BANNER);
-	    adView.setAdUnitId(AD_UNIT_ID);
-
-	    // Add the AdView to the view hierarchy. The view will have no size
-	    // until the ad is loaded.
-	    LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
-	    layout.addView(adView);
-
-	    // Create an ad request. Check logcat output for the hashed device ID to
-	    // get test ads on a physical device.
-	    AdRequest adRequest = new AdRequest.Builder()
-	        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-	        .addTestDevice("INSERT_YOUR_HASHED_DEVICE_ID_HERE")
-	        .build();
-
-	    // Start loading the ad in the background.
-	    adView.loadAd(adRequest);
-	  }
-
-	  @Override
-	  public void onResume() {
-	    super.onResume();
-	    if (adView != null) {
-	      adView.resume();
-	    }
-	  }
-
-	  @Override
-	  public void onPause() {
-	    if (adView != null) {
-	      adView.pause();
-	    }
-	    super.onPause();
-	  }
-
-	  /** Called before the activity is destroyed. */
-	  @Override
-	  public void onDestroy() {
-	    // Destroy the AdView.
-	    if (adView != null) {
-	      adView.destroy();
-	    }
-	    super.onDestroy();
-	  }
-	}
+//	/** The view to show the ad. */
+//	private AdView			  adView;
+//
+//	/* Your ad unit id. Replace with your actual ad unit id. */
+//	private static final String AD_UNIT_ID = "INSERT_YOUR_AD_UNIT_ID_HERE";
+//
+//	/** Called when the activity is first created. */
+//	@Override
+//	  public void onCreate(Bundle savedInstanceState) {
+//	    super.onCreate(savedInstanceState);
+//	    setContentView(R.layout.);
+//
+//	    // Create an ad.
+//	    adView = new AdView(this);
+//	    adView.setAdSize(AdSize.BANNER);
+//	    adView.setAdUnitId(AD_UNIT_ID);
+//
+//	    // Add the AdView to the view hierarchy. The view will have no size
+//	    // until the ad is loaded.
+//	    LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+//	    layout.addView(adView);
+//
+//	    // Create an ad request. Check logcat output for the hashed device ID to
+//	    // get test ads on a physical device.
+//	    AdRequest adRequest = new AdRequest.Builder()
+//	        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+//	        .addTestDevice("INSERT_YOUR_HASHED_DEVICE_ID_HERE")
+//	        .build();
+//
+//	    // Start loading the ad in the background.
+//	    adView.loadAd(adRequest);
+//	  }
+//
+//	@Override
+//	public void onResume() {
+//		super.onResume();
+//		if (adView != null) {
+//			adView.resume();
+//		}
+//	}
+//
+//	@Override
+//	public void onPause() {
+//		if (adView != null) {
+//			adView.pause();
+//		}
+//		super.onPause();
+//	}
+//
+//	/** Called before the activity is destroyed. */
+//	@Override
+//	public void onDestroy() {
+//		// Destroy the AdView.
+//		if (adView != null) {
+//			adView.destroy();
+//		}
+//		super.onDestroy();
+//	}
+}
