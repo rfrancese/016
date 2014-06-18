bf9dfeb8a325f1d2f88ec727e03ad27a047b4083
pinoelefante <pinoelefante@hotmail.it>
2014-06-18 14:18:48 +0200

    nome dns

--- src/it/unisa/mathchallenger/communication/Communication.java
+++ src/it/unisa/mathchallenger/communication/Communication.java
@@ -19,7 +19,8 @@ public class Communication implements Runnable {
 	private static Communication singleton;
 
 	private Socket			   socket;
-	private final static String  HOSTNAME	  = "54.76.113.193"; //ip del server
+	//private final static String  HOSTNAME	  = "54.76.113.193"; //ip del server
+	private final static String  HOSTNAME	  = "mathchallenger.servegame.com";
 	private final static int	 HOSTNAME_PORT = 50000;
 	private static int		   TIMEOUT_READ  = 10000;		// 10 secondi timeout
 	private static ThreadPing	t_ping;
