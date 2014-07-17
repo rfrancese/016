822f0317b778ab1374fd79a72214cdd68a73f3d5
Pino <pinoelefante@hotmail.it>
2014-07-17 14:28:22 +0200

    Stringa esternalizzata e cambio font per "Invio risposte"

--- src/it/unisa/mathchallenger/GiocaPartitaActivity.java
+++ src/it/unisa/mathchallenger/GiocaPartitaActivity.java
@@ -174,7 +174,8 @@ public class GiocaPartitaActivity extends Activity {
 			});
 		}
 		else {
-			domanda.setText("Invio risposte");
+			domanda.setTypeface(Typeface.DEFAULT);
+			domanda.setText(R.string.invioRisposte);
 			scriviDomanda();
 		}
 	}
